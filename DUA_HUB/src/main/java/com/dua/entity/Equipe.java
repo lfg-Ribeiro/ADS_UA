bc2b61797c92644aff751e9bc749f502baed3efa
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-09-20 11:07:09 -0300

    Att Entitys Equipe and Participante (lombok)

--- DUA_HUB/src/main/java/com/dua/entity/Equipe.java
+++ DUA_HUB/src/main/java/com/dua/entity/Equipe.java
@@ -1,10 +1,12 @@
 package com.dua.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.util.List;
 
 @Entity
 @Table(name = "equipes")
+@Data
 public class Equipe {
 
     @Id
@@ -22,37 +24,10 @@ public class Equipe {
     )
     private List<Participante> participantes;
 
-    // Construtor padrão (obrigatório para JPA)
     public Equipe() {}
 
-    // Construtor com parâmetros
     public Equipe(String nome, List<Participante> participantes) {
         this.nome = nome;
-        this.participantes = participantes;
-    }
-
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public List<Participante> getParticipantes() {
-        return participantes;
-    }
-
-    public void setParticipantes(List<Participante> participantes) {
         this.participantes = participantes;
     }
 }
--- DUA_HUB/src/main/java/com/dua/entity/Participante.java
+++ DUA_HUB/src/main/java/com/dua/entity/Participante.java
@@ -1,10 +1,12 @@
 package com.dua.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.util.List;
 
 @Entity
 @Table(name = "participantes")
+@Data
 public class Participante {
 
     @Id
@@ -20,45 +22,10 @@ public class Participante {
     @ManyToMany(mappedBy = "participantes")
     private List<Equipe> equipes;
 
-    // Construtor padrão
     public Participante() {}
 
-    // Construtor com parâmetros
     public Participante(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
-    }
-
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public List<Equipe> getEquipes() {
-        return equipes;
-    }
-
-    public void setEquipes(List<Equipe> equipes) {
-        this.equipes = equipes;
     }
 }
