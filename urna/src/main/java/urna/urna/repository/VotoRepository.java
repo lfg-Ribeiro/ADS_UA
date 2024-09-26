6ff83838f8c80ce61acd25491770ef813c3b001a
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-09-26 13:34:26 -0300

    Separando e corrigindo o local dos arquivos dentro do repositório

--- urna/src/main/java/urna/urna/repository/VotoRepository.java
+++
@@ -1,7 +0,0 @@
-package urna.urna.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import urna.urna.entity.Voto;
-
-public interface VotoRepository extends JpaRepository<Voto, Long> {
-}
