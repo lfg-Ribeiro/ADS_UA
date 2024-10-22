09804e5a81b8501b2b2792ac4133b75f661f420c
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-10-22 17:08:34 -0300

    criado a tela de login

--- 4o_periodo/cadastro-pessoas/src/app/components/login/login.component.ts
+++ 4o_periodo/cadastro-pessoas/src/app/components/login/login.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  user!: string;
+  pass!: string;
+
+  router = inject(Router);
+
+  logar() {
+    if (this.user == 'admin' && this.pass == 'admin'){
+      this.router.navigate(['/admin/pessoas']);
+
+      Swal.fire({
+        title: 'Acesso Liberado!',
+        icon: 'success',
+        confirmButtonText: 'Prosseguir'
+      });
+    }
+  }
 }
