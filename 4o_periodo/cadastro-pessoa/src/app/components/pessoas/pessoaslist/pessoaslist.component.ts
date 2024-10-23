795228efcb95c2dfe8dbd6db83ecfddc66afe398
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-10-23 16:17:00 -0300

    :white_check_mark: Página da Listagem de Pessoas

--- 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
+++ 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
@@ -1,46 +1,30 @@
 import { Component } from '@angular/core';
 import { Pessoa } from '../../../models/pessoa';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pessoaslist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './pessoaslist.component.html',
-  styleUrl: './pessoaslist.component.scss'
+  styleUrl: './pessoaslist.component.scss',
 })
 export class PessoaslistComponent {
-
   lista: Pessoa[] = [];
 
   constructor() {
-    let pessoa: Pessoa = new Pessoa();
-    pessoa.id = 1;
-    pessoa.nome = 'Ribeiro';
-    pessoa.documento = '12345678900';
-    pessoa.email = 'ribas@ribas.com';
+    this.lista.push(new Pessoa(1, 'Ribeiro', '12345678900', 'ribas@ribas.com'));
+    this.lista.push(new Pessoa(2, 'Nicholas', '45678900123', 'tcholas@tcholas.com'));
+    this.lista.push(new Pessoa(3, 'Hoesel', '78900123456', 'hoesa@hoesa.com'));
+    this.lista.push(new Pessoa(4, 'Inácio', '00123456789', 'nana@nana.com'));
+  }
 
-    let pessoa2: Pessoa = new Pessoa();
-    pessoa2.id = 2;
-    pessoa2.nome = 'Nicholas';
-    pessoa2.documento = '45678900123';
-    pessoa2.email = 'tcholas@tcholas.com';
+  editar(){
+  
+    }
 
-    let pessoa3: Pessoa = new Pessoa();
-    pessoa3.id = 3;
-    pessoa3.nome = 'Hoesel';
-    pessoa3.documento = '78900123456';
-    pessoa3.email = 'hoesa@hoesa.com';
+  deletar(){
 
-    let pessoa4: Pessoa = new Pessoa();
-    pessoa4.id = 4;
-    pessoa4.nome = 'Inácio';
-    pessoa4.documento = '00123456789';
-    pessoa4.email = 'nana@nana.com';
-
-    this.lista.push(pessoa);
-    this.lista.push(pessoa2);
-    this.lista.push(pessoa3);
-    this.lista.push(pessoa4);
-}
+  }
 }
--- 4o_periodo/cadastro-pessoa/src/app/models/pessoa.ts
+++ 4o_periodo/cadastro-pessoa/src/app/models/pessoa.ts
@@ -3,4 +3,11 @@ export class Pessoa {
     nome!: string;
     documento!: string;
     email!: string;
+
+    constructor(id: number, nome: string, documento: string, email: string) {
+        this.id = id;
+        this.nome = nome;
+        this.documento = documento;
+        this.email = email;
+    }
 }
