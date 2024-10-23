006e974fe18c027992855e2445355015a01cc072
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-10-23 17:30:43 -0300

    Funções de Editar, Adicionar e Excluir configuradas

--- 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoasdetails/pessoasdetails.component.ts
+++ 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoasdetails/pessoasdetails.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Pessoa } from '../../../models/pessoa';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pessoasdetails',
   standalone: true,
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './pessoasdetails.component.html',
   styleUrl: './pessoasdetails.component.scss'
 })
 export class PessoasdetailsComponent {
+
+  pessoa: Pessoa = new Pessoa(0, "", "", "");
+  router = inject(ActivatedRoute);
+  router2 = inject(Router);
+
+  constructor() { 
+    let id = this.router.snapshot.params['id'];
+    if(id > 0){
+      this.findById(id);
+   }
+  }
+
+   findById(id: number){
+    let pessoaRetornado: Pessoa = new Pessoa(id, "Fulano Teste", "11122233344", "fulano@fulano.com");
+    this.pessoa = pessoaRetornado;
+   }
+
+  salvar(){
+    if(this.pessoa.id > 0){
+      alert('Editado com sucesso!');
+      this.router2.navigate(['/admin/pessoas'], { state: { pessoaEditada: this.pessoa } });
+    }else{
+    alert('Salvo com sucesso!');
+    this.router2.navigate(['/admin/pessoas'], { state: { pessoaNova: this.pessoa } });
+  }
+  }
 
 }
--- 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
+++ 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
@@ -18,13 +18,27 @@ export class PessoaslistComponent {
     this.lista.push(new Pessoa(2, 'Nicholas', '45678900123', 'tcholas@tcholas.com'));
     this.lista.push(new Pessoa(3, 'Hoesel', '78900123456', 'hoesa@hoesa.com'));
     this.lista.push(new Pessoa(4, 'Inácio', '00123456789', 'nana@nana.com'));
-  }
-
-  editar(){
   
+    let pessoaNova = history.state.pessoaNova; 
+    let pessoaEditada = history.state.pessoaEditada;
+
+    if (pessoaNova){
+      pessoaNova.id = this.lista.length + 1;
+      this.lista.push(pessoaNova);
     }
+    if (pessoaEditada){
+      let indice = this.lista.findIndex(x => {return x.id == pessoaEditada.id});
+      this.lista[indice] = pessoaEditada;
 
-  deletar(){
-
+    }
+  
+  
+  }
+  
+  deleteById(pessoa: Pessoa){
+    if (confirm('Deseja realmente excluir?')){
+    let indice = this.lista.findIndex(x => {return x.id == pessoa.id});
+    this.lista.splice(indice, 1);
+    }
   }
 }
