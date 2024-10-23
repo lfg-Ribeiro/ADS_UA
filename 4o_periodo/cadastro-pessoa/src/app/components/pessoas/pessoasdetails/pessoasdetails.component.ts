b07483f3e06d7107b2ec76b99558ce32752c805e
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-10-23 18:00:13 -0300

    complementado sweet alert no projeto

--- 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoasdetails/pessoasdetails.component.ts
+++ 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoasdetails/pessoasdetails.component.ts
@@ -3,40 +3,56 @@ import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Pessoa } from '../../../models/pessoa';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pessoasdetails',
   standalone: true,
   imports: [MdbFormsModule, FormsModule],
   templateUrl: './pessoasdetails.component.html',
-  styleUrl: './pessoasdetails.component.scss'
+  styleUrl: './pessoasdetails.component.scss',
 })
 export class PessoasdetailsComponent {
-
-  pessoa: Pessoa = new Pessoa(0, "", "", "");
+  pessoa: Pessoa = new Pessoa(0, '', '', '');
   router = inject(ActivatedRoute);
   router2 = inject(Router);
 
-  constructor() { 
+  constructor() {
     let id = this.router.snapshot.params['id'];
-    if(id > 0){
+    if (id > 0) {
       this.findById(id);
-   }
+    }
   }
 
-   findById(id: number){
-    let pessoaRetornado: Pessoa = new Pessoa(id, "Fulano Teste", "11122233344", "fulano@fulano.com");
+  findById(id: number) {
+    let pessoaRetornado: Pessoa = new Pessoa(
+      id,
+      'Fulano Teste',
+      '11122233344',
+      'fulano@fulano.com'
+    );
     this.pessoa = pessoaRetornado;
-   }
-
-  salvar(){
-    if(this.pessoa.id > 0){
-      alert('Editado com sucesso!');
-      this.router2.navigate(['/admin/pessoas'], { state: { pessoaEditada: this.pessoa } });
-    }else{
-    alert('Salvo com sucesso!');
-    this.router2.navigate(['/admin/pessoas'], { state: { pessoaNova: this.pessoa } });
-  }
   }
 
+  salvar() {
+    if (this.pessoa.id > 0) {
+      Swal.fire({
+        title: 'Editado com sucesso',
+        icon: 'success',
+        confirmButtonText: 'OK',
+      });
+      this.router2.navigate(['/admin/pessoas'], {
+        state: { pessoaEditada: this.pessoa },
+      });
+    } else {
+      Swal.fire({
+        title: 'Salvo com sucesso',
+        icon: 'success',
+        confirmButtonText: 'OK',
+      });
+      this.router2.navigate(['/admin/pessoas'], {
+        state: { pessoaNova: this.pessoa },
+      });
+    }
+  }
 }
--- 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
+++ 4o_periodo/cadastro-pessoa/src/app/components/pessoas/pessoaslist/pessoaslist.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Pessoa } from '../../../models/pessoa';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pessoaslist',
@@ -18,27 +19,43 @@ export class PessoaslistComponent {
     this.lista.push(new Pessoa(2, 'Nicholas', '45678900123', 'tcholas@tcholas.com'));
     this.lista.push(new Pessoa(3, 'Hoesel', '78900123456', 'hoesa@hoesa.com'));
     this.lista.push(new Pessoa(4, 'Inácio', '00123456789', 'nana@nana.com'));
-  
-    let pessoaNova = history.state.pessoaNova; 
+
+    let pessoaNova = history.state.pessoaNova;
     let pessoaEditada = history.state.pessoaEditada;
 
-    if (pessoaNova){
+    if (pessoaNova) {
       pessoaNova.id = this.lista.length + 1;
       this.lista.push(pessoaNova);
     }
-    if (pessoaEditada){
-      let indice = this.lista.findIndex(x => {return x.id == pessoaEditada.id});
+    if (pessoaEditada) {
+      let indice = this.lista.findIndex((x) => {
+        return x.id == pessoaEditada.id;
+      });
       this.lista[indice] = pessoaEditada;
-
     }
-  
-  
   }
-  
-  deleteById(pessoa: Pessoa){
-    if (confirm('Deseja realmente excluir?')){
-    let indice = this.lista.findIndex(x => {return x.id == pessoa.id});
-    this.lista.splice(indice, 1);
-    }
+
+  deleteById(pessoa: Pessoa) {
+    Swal.fire({
+      title: 'Tem certeza que deseja deletar o usuário?',
+      icon: 'warning',
+      showConfirmButton: true,
+      confirmButtonText: 'Sim',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let indice = this.lista.findIndex((x) => {
+          return x.id == pessoa.id;
+        });
+        this.lista.splice(indice, 1);
+
+        Swal.fire({
+          title: 'Deletado com sucesso',
+          icon: 'success',
+          confirmButtonText: 'OK',
+        });
+      }
+    });
   }
 }
