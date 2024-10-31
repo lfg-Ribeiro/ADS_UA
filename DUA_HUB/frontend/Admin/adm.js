442e0cbb56fda4d13d9f394e3d70fc9f0b544674
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-10-31 17:11:02 -0300

    Excluindo antigo front-end

--- DUA_HUB/frontend/Admin/adm.js
+++
@@ -1,9 +0,0 @@
-function checklogin(){
-
-    const isLogged=localStorage.getItem(`logged`)
-    if (isLogged!==`true`)
-     window.location.href=`../Login/login.html`
-    
-}
-
-window.onload=checklogin
--- DUA_HUB/frontend/Login/login.js
+++
@@ -1,8 +0,0 @@
-function redirectToRegister(){
-    window.location.href=`../Registro/regis.html`
-}
-
-function login(){
-    localStorage.setItem(`logged`, `true`)
-    window.location.href=`../Admin/adm.html`
-}
