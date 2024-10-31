05a89f0d3ed74049e29f00adfbfccdeca2f48014
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-10-31 20:57:22 -0300

    criado a página main, ajustado a navbar e criado a página principal do amd

--- DUA_HUB/frontend/src/app/components/layout/main/main.component.ts
+++ DUA_HUB/frontend/src/app/components/layout/main/main.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
-
+  
 }
