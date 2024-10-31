3b1da04ebb6d8d35a607ca1b8756cd21fc06a4f7
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-10-31 17:51:53 -0300

    Criado as pastas para as telas do front

---
+++ DUA_HUB/frontend/src/app/components/equipes/equipes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipes',
+  standalone: true,
+  imports: [],
+  templateUrl: './equipes.component.html',
+  styleUrl: './equipes.component.scss'
+})
+export class EquipesComponent {
+
+}
---
+++ DUA_HUB/frontend/src/app/components/layout/inscricao/inscricao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscricao',
+  standalone: true,
+  imports: [],
+  templateUrl: './inscricao.component.html',
+  styleUrl: './inscricao.component.scss'
+})
+export class InscricaoComponent {
+
+}
