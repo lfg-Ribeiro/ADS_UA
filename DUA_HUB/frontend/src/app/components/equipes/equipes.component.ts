8cb765d7a8b80a64953b049c5cc9280a45864f4e
lfg-Ribeiro <luanfp2004@hotmail.com>
2024-11-02 00:18:29 -0300

    criado a tela de equipes

--- DUA_HUB/frontend/src/app/components/equipes/equipes.component.ts
+++ DUA_HUB/frontend/src/app/components/equipes/equipes.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Equipe } from '../../models/equipes/equipe';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-equipes',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './equipes.component.html',
-  styleUrl: './equipes.component.scss'
+  styleUrl: './equipes.component.scss',
 })
 export class EquipesComponent {
+  lista: Equipe[] = [];
 
+  findAll() {}
+
+  deleteById(equipe: Equipe) {
+    Swal.fire({
+      title: 'Tem certeza que deseja deletar a equipe?',
+      icon: 'warning',
+      showConfirmButton: true,
+      confirmButtonText: 'Sim',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        let indice = this.lista.findIndex((x) => {
+          return x.id == equipe.id;
+        });
+        this.lista.splice(indice, 1);
+
+        Swal.fire({
+          title: 'Equipe deletada com sucesso',
+          icon: 'success',
+          confirmButtonText: 'OK',
+        });
+      }
+    });
+  }
 }
